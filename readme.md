5627e9ecc4ced5d23efae0f6359fc37261f23471
Sophia Hage <sophia@sophiah.gay>
2024-01-25 09:28:03 +0100

    Write dripfeed server

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Dripfeed
+
+Just a quick web server that drip feeds a file to a client.
+
+Not async or multi-threaded, just a quick way to test timeouts and such.
---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:?}", http_request);
+
+    // Send the file "img.png"
+    let file = std::fs::read("src/img.png").unwrap();
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
+    // Slowly send the file over a span of some time
+    let time_in_seconds = 30;
+    let size = file.len();
+
+    let chunk_size = size / time_in_seconds;
+    let chunk_size = ceil(chunk_size);
+
+    let chunks = size / chunk_size;
+    let chunks = ceil(chunks);
+
+    println!("Sending {} chunks of size {}", chunks, chunk_size);
+
+    for chunk in file.chunks(chunk_size) {
+        stream.write_all(chunk).unwrap();
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
+
+    println!("File sent!");
+}
+
+fn ceil(x: usize) -> usize {
+    if x % 2 != 0 {
+        x + 1
+    } else {
+        x
+    }
+}
